e9c06863b85f3b2ccce310eb11dcf4e0d4fd8074
Mathis Goichon <mathisgoichon@gmail.com>
2022-06-08 08:34:23 +0200

    chore(git): remove cmake-build-debug from repo
    Signed-off-by: Mathis Goichon <mathisgoichon@gmail.com>

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/pauls/CLionProjects/CompilerC
-# Build directory: /mnt/c/Users/pauls/CLionProjects/CompilerC/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("runtime")
-subdirs("compiler")
