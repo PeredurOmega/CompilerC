720bd0b015ca846f1d3ea494c2213c689efb4afa
Mathis Goichon <mathisgoichon@gmail.com>
2022-06-14 13:31:51 +0200

    test: add some tests

---
+++ tests/testfiles/7_return_addsub.c
@@ -0,0 +1,5 @@
+int main() {
+    int a = 8, b = 2, c = 4, d = 6, res;
+    res = a - b + 5;
+    return 5 + res - c + d;
+}
---
+++ tests/testfiles/9_return_addsub_priority.c
@@ -0,0 +1,5 @@
+int main() {
+    int a = 8, b = 2, c = 4, d = 6, res;
+    res = a - (b + 1) + 5 + (6 + 4 - 2) - (1 - 6 - (b + c + d));
+    return res - (c + d + a + 2 + 3 + res + d + 10 + 3 + 7 + 9 + 7 + 4 + d);
+}
