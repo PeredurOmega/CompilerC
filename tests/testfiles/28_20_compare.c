5796e1516480166bd89afcdb6040f17c495b1905
Mathis Goichon <mathisgoichon@gmail.com>
2022-06-15 11:39:04 +0200

    test: add various tests

---
+++ tests/testfiles/28_20_compare.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int a = 32;
+    int b = 0;
+    int c = ((a < b) != 56 > 4 == (a != b));
+    return b;
+}
---
+++ tests/testfiles/29_3_grammar_error.c
@@ -0,0 +1,5 @@
+int main() {
+    int a;
+    a = "toto" + "tata";
+    return a;
+}
