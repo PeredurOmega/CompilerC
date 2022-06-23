73228f392779d3ea6fa2c51e20b5bd25b4b65efe
Emmanuel Garreau <manu.garreau@gmail.com>
2022-06-23 10:51:34 +0200

    test: add while tests

---
+++ tests/testfiles/branchs/complex_while_2.c
@@ -0,0 +1,12 @@
+int main(){
+    int count1 = 1;
+    int count2 = 1;
+    while (count1 != 4)
+    {
+        while(count2 != 4){
+            count2++;
+        }
+        count1++;
+    }
+    return count1*count2;
+}
